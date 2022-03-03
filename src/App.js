1544d71b96069492b5fa300be08d1430ac16c8bc
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2022-03-02 19:48:41 -0600

    functioning concept, no data, no styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import Accordion from "./components/Accordion";
+import category1 from "../src/utils/category1.json"
+import Heading from "./components/Heading";
 
 function App() {
   return (
     <div>
-     <h1>
-      Hello Frikin World
-     </h1>  
+      <h1>ZOX FAQs</h1>
+      <Heading heading="heading1" />
+      <div className="accordion">
+        {category1.map(({ id, title, content }) => (
+          <Accordion key={id} title={title} content={content} />
+        ))}
+      </div>
     </div>
   );
 }
